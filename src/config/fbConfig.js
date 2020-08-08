9bed418bda950a1e606ea043ed4d029189d443c0
Rkverma1996 <sweetsanjeevkumar96@gmail.com>
2020-08-08 22:25:15 +0530

    added firebase config setting

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD5dDVNxQjrN7wyvaTLwv6cHppnxs1wfPM",
+  authDomain: "react-firebase-mario.firebaseapp.com",
+  databaseURL: "https://react-firebase-mario.firebaseio.com",
+  projectId: "react-firebase-mario",
+  storageBucket: "react-firebase-mario.appspot.com",
+  messagingSenderId: "148140898504",
+  appId: "1:148140898504:web:8f50dba57c4cacaf774dfd",
+  measurementId: "G-2GHSZ06BND"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
