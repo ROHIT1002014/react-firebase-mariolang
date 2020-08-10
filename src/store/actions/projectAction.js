2f77ae674bfde828b6f55d7054df43874bd77078
Rkverma1996 <sweetsanjeevkumar96@gmail.com>
2020-08-10 15:44:55 +0530

    make button workable to update firestore

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,9 +1,24 @@
 export const createProject = (project) => {
   return (dispatch, getState, { getFirestore, getFirebase } ) => {
-    // make async call to DB
+    const firestore = getFirestore(); // it will give firestore reference
+    firestore.collection('projects').add({
+      ...project, // it will dispaly add element of state
+      authorFirstName : 'Roiht',
+      authorLastName : 'verma',
+      authorId : 123456,
+      createdAt: new Date()
+    }).then(() => {
+       // make async call to DB
     dispatch({
       type: 'CREATE_PROJECT',
       project  //this is being send
+    });
+    }).catch(err => {
+       // make async call to DB
+    dispatch({
+      type: 'CREATE_PROJECT_ERROR',
+      err  //this is being send
+    });
     });
   }
 }
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -12,7 +12,14 @@ const initState = {
 
 const  projectReducer = (state = initState, action) => {
   switch(action.type) {
-    case 'CREATE_PROJECT': console.log('proejct created', action.project)
+    case 'CREATE_PROJECT':
+      console.log('proejct created', action.project);
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create proejct error', action.err);
+      return state;
+    default:
+      return state;
   }
 return state;
 }
