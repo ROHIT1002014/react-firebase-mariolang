4f586e71f947306d15d645e901641d97a9a24354
Rkverma1996 <sweetsanjeevkumar96@gmail.com>
2020-08-10 14:21:44 +0530

    used redux-firestore add react-redux-fireabse

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './store/reducers/rootReducer';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import fbConfig from './config/fbConfig';
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(
+  rootReducer,
+  compose(
+  applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))),
+  reactReduxFirebase(fbConfig),
+  reduxFirestore(fbConfig)
+  )
 
 ReactDOM.render(
   <React.StrictMode>
